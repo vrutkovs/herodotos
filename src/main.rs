a985f03edb4f5cb45fddff5faefe34c379fb575b
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-03-18 02:24:52 +0100

    Split into PMHandler funcs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use slack;
 use std::collections::HashMap;
 
+struct MsgData {
+  user: String,
+  text: String,
+}
+
 struct PMHandler {
   daily_statuses: HashMap<String, Vec<String>>,
 }
@@ -9,44 +14,22 @@ struct PMHandler {
 impl slack::EventHandler for PMHandler {
   fn on_event(&mut self, cli: &slack::RtmClient, event: slack::Event) {
     println!("on_event(event: {:?})", event);
-    let slack_message = match event {
-      slack::Event::Message(m) => Box::leak(m),
-      _ => return,
-    };
-    let m = match &slack_message {
-      slack::Message::Standard(m) => m,
-      _ => return,
-    };
-    let text = match &m.text {
-      Some(t) => t,
+    let msg_data = match self.get_message(event) {
+      Some(m) => m,
       None => return,
     };
-    let user = match &m.user {
-      Some(u) => u,
-      None => return,
-    };
-    println!("{}: '{}'", user, text);
+    println!("{}: '{}'", msg_data.user, msg_data.text);
 
     // if text == "done" then post the collected messages to the channel:
-    match text.as_str() {
-      "done" => {
+    //
+    //
+
+    match self.process_message(msg_data) {
+      Some(message) => {
         let channel_id = "DND47PSF9";
-        let debug_output = format!("{:?}", self.daily_statuses);
-        let _ = cli
-          .sender()
-          .send_message(&channel_id, debug_output.as_str());
+        let _ = cli.sender().send_message(&channel_id, message.as_str());
       }
-      _ => {
-        // Store messages
-        match self.daily_statuses.get_mut(user) {
-          Some(usr_status) => usr_status.push(text.to_string()),
-          None => {
-            self
-              .daily_statuses
-              .insert(user.to_string(), vec![text.to_string()]);
-          }
-        }
-      }
+      None => return,
     }
   }
 
@@ -58,6 +41,52 @@ impl slack::EventHandler for PMHandler {
   fn on_connect(&mut self, cli: &slack::RtmClient) {
     println!("on_connect");
     //TODO: set online signal
+  }
+}
+
+impl PMHandler {
+  fn get_message(&mut self, event: slack::Event) -> Option<MsgData> {
+    let slack_message = match event {
+      slack::Event::Message(m) => Box::leak(m),
+      _ => return None,
+    };
+    let m = match &slack_message {
+      slack::Message::Standard(m) => m,
+      _ => return None,
+    };
+    let text = match &m.text {
+      Some(t) => t,
+      None => return None,
+    };
+    let user = match &m.user {
+      Some(u) => u,
+      None => return None,
+    };
+    Some(MsgData {
+      text: text.to_string(),
+      user: user.to_string(),
+    })
+  }
+
+  fn process_message(&mut self, msg: MsgData) -> Option<String> {
+    match msg.text.as_str() {
+      "done" => Some(format!("{:?}", self.daily_statuses)),
+      _ => {
+        // Store messages
+        match self.daily_statuses.get_mut(&msg.user) {
+          Some(usr_status) => {
+            usr_status.push(msg.text.to_string());
+            None
+          }
+          None => {
+            self
+              .daily_statuses
+              .insert(msg.user.to_string(), vec![msg.text.to_string()]);
+            None
+          }
+        }
+      }
+    }
   }
 }
 
