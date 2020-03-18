e51f7cde488badea884f0d53cbbb48d04760eb65
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-03-18 01:25:51 +0100

    Fetch text from PM message

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 extern crate slack;
 
-use slack::{Event, RtmClient};
+use slack::{Event, Message, RtmClient};
 
-struct MyHandler;
+struct PMHandler;
 
 #[allow(unused_variables)]
-impl slack::EventHandler for MyHandler {
+impl slack::EventHandler for PMHandler {
   fn on_event(&mut self, cli: &RtmClient, event: Event) {
     println!("on_event(event: {:?})", event);
+    let m = match event {
+      Event::Message(m) => Box::leak(m),
+      _ => return,
+    };
+    let text = match &m {
+      Message::Standard(m) => match &m.text {
+        Some(t) => t,
+        None => return,
+      },
+      _ => return,
+    };
+    println!("{}", text)
   }
 
   fn on_close(&mut self, cli: &RtmClient) {
@@ -25,7 +37,7 @@ fn main() {
     0 | 1 => panic!("No api-key in args! Usage: cargo run -- <api-key>"),
     x => args[x - 1].clone(),
   };
-  let mut handler = MyHandler;
+  let mut handler = PMHandler;
   let r = RtmClient::login_and_run(&api_key, &mut handler);
   match r {
     Ok(_) => {}
